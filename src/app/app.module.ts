f01dd6a7a53a30defca7b212e14abb0935854064
cheyycheyy <woshicheyy@sina.com>
2017-09-17 14:27:11 +0800

    增加组件，增加jquery，搭建框架

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { ContentComponent } from './content/content.component';
+import { StartsComponent } from './starts/starts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarouselComponent,
+    ContentComponent,
+    StartsComponent
   ],
   imports: [
     BrowserModule
