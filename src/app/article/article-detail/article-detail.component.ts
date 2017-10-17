574f08723488eb2aaef5b748ec0d42c4edbdd863
woshicheyy@sina.com <woshicheyy@sina.com>
2017-10-17 19:02:24 +0800

    修改html注入问题

--- src/app/article/article-detail/article-detail.component.ts
+++ src/app/article/article-detail/article-detail.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, ParamMap, Router} from '@angular/router';
+import {ActivatedRoute, ParamMap, Params, Router} from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 import {Article} from '../../model/article.model';
 import {ArticleService} from '../../shared/article.service';
@@ -21,15 +21,17 @@ export class ArticleDetailComponent implements OnInit {
 
     ngOnInit() {
         scroll(0, 0);
-        this.route.paramMap
-            .switchMap((params: ParamMap) =>
-                params.get('id')).subscribe((id: string) => {
-            this.id = 12;
-
+        this.route.params.subscribe((params: Params) => {
+            this.id = params['id'];
+            console.log('cheyy' + this.id);
             this.articleService.getArticle(this.id, (article) => {
                 this.article = article;
+                console.log(this.article.body);
+                document.getElementById('article1').innerHTML = this.article.body;
+                // console.log(this.article.body);
             });
         });
+
 
     }
 
